e6c4decf30888a6df8c607fa84df406174cfe4ba
Melvin <melvin.sy@outlook.com>
2024-06-16 20:19:38 +0800

    Exclude .gitignore from dotfiles initialization

--- fresh.sh
+++ fresh.sh
@@ -3,7 +3,7 @@
 echo "Setting up your Mac..."
 
 echo "Creating dotfiles symlink..."
-for dotfile in `find . -type f -name '.*'`; do
+for dotfile in `find . -type f -name '.*' -not -name '.gitignore'`; do
     if [ ! -f $HOME/$dotfile ]; then
       echo "Creating symlink for $dotfile"
       ln -s $(pwd)/$dotfile $HOME
